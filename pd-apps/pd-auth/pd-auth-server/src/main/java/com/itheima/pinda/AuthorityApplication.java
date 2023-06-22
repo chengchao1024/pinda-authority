0ee39e3318cfcf718b7c36e74ee3e5788a58a0ed
chengchao <chengchaomvp@163.com>
2023-06-22 18:43:59 +0800

    Initial commit

---
+++ pd-apps/pd-auth/pd-auth-server/src/main/java/com/itheima/pinda/AuthorityApplication.java
@@ -0,0 +1,11 @@
+package com.itheima.pinda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityApplication.class,args);
+    }
+}
